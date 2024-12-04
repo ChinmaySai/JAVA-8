f426d26b2b50a5e6d452b999b31c4c5707d66efa
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2024-12-04 09:04:58 +0530

    Committing the Function Functional interface Code

---
+++ Java8/src/FunctionFunctionalLamda.java
@@ -0,0 +1,9 @@
+import java.util.function.Function;
+public class FunctionFunctionalLamda {
+
+	public static void main(String[] args) {
+
+		Function<String,Integer> fun= name ->name.length();
+		System.out.println("Lenght of String "+fun.apply("Chinmay"));
+	}
+}
--- Java8/src/PredicateFunctionalInterfaceExample2.java
+++ Java8/src/PredicateFunctionalInterfaceExample2.java
@@ -11,7 +11,12 @@ public class PredicateFunctionalInterfaceExample2 {
       Predicate<Integer> p2=i->i%2==0;
       System.out.println("Elements divisible by 2");
       m1(p2,a);
-      
+      System.out.println("Elements greater than 10 and divisible by 2");
+      m1(p1.and(p2),a);
+      System.out.println("Elements greater than 10 or divisible by 2");
+      m1(p1.or(p2),a);
+      System.out.println("Elements which are odd");
+      m1(p2.negate(),a);
       
 	}
 	
