2cdb5b63a0943f7cf16ee8d6ce227a2f11d79aca
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2024-12-07 15:04:13 +0530

    Functional Commit

--- Java8/src/FunctionFunctionalLamda.java
+++ Java8/src/FunctionFunctionalLamda.java
@@ -2,6 +2,7 @@ import java.util.function.Function;
 public class FunctionFunctionalLamda {
 
 	public static void main(String[] args) {
+		
 
 		Function<String,Integer> fun= name ->name.length();
 		System.out.println("Lenght of String "+fun.apply("Chinmay"));
