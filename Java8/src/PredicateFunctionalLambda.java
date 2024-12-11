ce6aa7d096448255f7884eb4fd40b861cada6a1b
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2024-12-11 08:35:03 +0530

    Commiting the changes

--- Java8/src/PredicateFunctionalLambda.java
+++ Java8/src/PredicateFunctionalLambda.java
@@ -7,7 +7,7 @@ public class PredicateFunctionalLambda {
 		Predicate<Integer> p= i->i>10;
 		System.out.println(p.test(10));
 		System.out.println(p.test(20));
-		
+
 		Predicate<String> p1=(name)-> name.length()>3;
 		System.out.println(p1.test("Chinmay"));
 		System.out.println(p1.test("Sai"));
