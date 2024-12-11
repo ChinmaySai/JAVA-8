41a7d550c8e945ecb58543d412ad88f270434352
Chinmay Sai <41532698+ChinmaySai@users.noreply.github.com>
2024-12-11 14:42:29 +0530

    Deleted

--- Java8/src/FunctionFunctionalLamda.java
+++
@@ -1,9 +0,0 @@
-import java.util.function.Function;
-public class FunctionFunctionalLamda {
-
-	public static void main(String[] args) {
-		
-		Function<String,Integer> fun= name ->name.length();
-		System.out.println("Lenght of String "+fun.apply("Chinmay"));
-	}
-}
