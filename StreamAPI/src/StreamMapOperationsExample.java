fb4807647fc24217910d4bd807cfd7127723be1b
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2025-01-01 20:09:32 +0530

    Stream API map method example Changes Commit.

---
+++ StreamAPI/src/StreamMapOperationsExample.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamMapOperationsExample {
+
+	public static void main(String[] args) {
+		List<String> department=new ArrayList<>();
+		department.add("Supply");
+		department.add("HR");
+		department.add("Sales");
+		department.add("Marketing");
+		//Stream<String> dep=department.stream();
+		System.out.println("Department List : \n");
+		department.stream().map(text->text.toUpperCase()).forEach(System.out::println);
+		System.out.println();
+		System.out.println("Second Usage of map method of Stream API \n");
+		department.stream().map(text->text.toUpperCase()).map(text->"Department - > "+text).forEach(System.out::println);
+
+		
+		
+	}
+
+}
