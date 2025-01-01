4fafd2927a16524e8876ea7192a87aabcf35e774
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2025-01-01 19:08:10 +0530

    Committing Stream API Example 1 Code Changes.

---
+++ StreamAPI/src/StreamExample1.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamExample1 {
+
+	public static void main(String[] args) {
+
+		
+		List<String> department=new ArrayList<>();
+		department.add("Supply");
+		department.add("HR");
+		department.add("Sales");
+		department.add("Marketing");
+		Stream<String> dep=department.stream();
+		dep.forEach(depart ->System.out.println(depart));
+		System.out.println();
+		Stream<String> newStream=Stream.of("Chinmay","Java 8");
+	     newStream.forEach(System.out::println);
+	    System.out.println();
+	     System.out.println("Paraller Stream Output : ");
+	    Stream<String> parallelStream=department.parallelStream();
+	    parallelStream.forEach(System.out::println);
+
+	}
+
+}
