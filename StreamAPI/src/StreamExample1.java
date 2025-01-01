b5df73b522c3328a30a3a167a532ea25489b172e
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2025-01-01 19:46:31 +0530

    Commented added for Understanding of Stream.of Method.

--- StreamAPI/src/StreamExample1.java
+++ StreamAPI/src/StreamExample1.java
@@ -15,7 +15,7 @@ public class StreamExample1 {
 		Stream<String> dep=department.stream();
 		dep.forEach(depart ->System.out.println(depart));
 		System.out.println();
-		Stream<String> newStream=Stream.of("Chinmay","Java 8");
+		Stream<String> newStream=Stream.of("Chinmay","Java 8");//These elements will be internally Stored in Array
 	     newStream.forEach(System.out::println);
 	    System.out.println();
 	     System.out.println("Paraller Stream Output : ");
